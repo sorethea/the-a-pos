c62d03e1e2bcbc11e43727eb952ce9627982fcfc
Sorethea OK <sorethea@gmail.com>
2024-08-26 16:28:21 +0700

    update react structure

--- resources/dist/src/components/auth/Login.js
+++ resources/dist/src/components/auth/Login.js
@@ -206,10 +206,9 @@ const Login = () => {
                                         </span>
                                     )}
                                 </button>
-                                <br/>
                                 <Link
                                     to="#"
-                                    className="link-info fs-6 text-decoration-none"
+                                    className="link-info mt-2 fs-6 text-decoration-none"
                                 >
                                     {getFormattedMessage(
                                         "login-form.register.label"
